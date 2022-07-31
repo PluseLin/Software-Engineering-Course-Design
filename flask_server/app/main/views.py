cdca2b44d2bd1c5b4ef9077925e0202c5a70a9ae
PluseLin <1390297535@qq.com>
2022-07-31 17:17:11 +0800

    add 4 api,for register,login,update and query

--- flask_server/app/main/views.py
+++ flask_server/app/main/views.py
@@ -1,4 +1,96 @@
+from tenacity import retry
 from . import main
 from ..dbAPI import *
 from flask import *
+import time
 
+@main.route("/userLogin",methods=['POST'])
+def login():
+    ret_data={
+        "iscorrect":False,
+        "message":"",
+        "url":"",
+    }
+    if(request.method=='POST'):
+        req_data=request.get_json()
+        username,password=req_data["username"],req_data["password"]
+        if User_Query_by_username_and_password(username,password) is None:
+            req_data["message"]="用户名或密码错误"
+            req_data["url"]="#"
+        else:
+            session["username"]=True
+            req_data["iscorrect"]=True
+            req_data["url"]="/{}/index".format("") #可能需要修改
+    return jsonify(ret_data)
+
+@main.route("/userRegister",methods=['POST'])
+def register():
+    ret_data={
+        "iscorrect":False,
+        "message":""
+    }
+    if(request.method=='POST'):
+        req_data=request.get_json()
+        username=req_data["username"]
+        password,repassword=req_data["password"],req_data["repassword"]
+        #先检查用户名是否重复
+        if User_Query_by_username(username) is not None:
+            req_data["message"]="用户名已被使用！"
+        #再检查两次密码是否一致
+        elif password!=repassword:
+            req_data["message"]="两次密码不一致！"
+        else:
+            #注册成功
+            user=User(
+                username=username,
+                password=password,
+                email=req_data["email"],
+                reg_time=time.strftime("%Y-%m-%d %h:%M:%S",time.localtime())
+            )
+            User_Insert(user)
+            req_data["iscorrect"]=True
+    return jsonify(ret_data)
+
+@main.route("/getUserInfo_<username>",methods=['GET'])
+def getUserInfo(username):
+    ret_data={
+        "id":0,
+        "username":username,
+        "password":"",
+        "email":"",
+        "regtime":"",
+        "ip_addr":request.remote_addr
+    }
+    if request.method=='GET':
+        user=User_Query_by_username(username)
+        if user is not None:
+            ret_data["password"]=user.password
+            ret_data["email"]=user.email
+            ret_data["regtime"]=user.reg_time
+            ret_data["id"]=user.id
+    return jsonify(ret_data)
+
+@main.route("/UpdateUserInfo",methods=['PUT'])
+def UpdateUserInfo(username):
+    ret_data={
+        "iscorrect":False,
+        "message":""
+    }
+    if request.method=="PUT":
+        req_data=request.get_json()
+        user_id=req_data["username"]
+        user=User_Query_by_id(user_id)
+        #首先，保证用户id存在
+        if user is None:
+            ret_data["message"]="用户不存在！"
+        #其次，保证新用户名存在
+        elif User_Query_by_username(username) is not None:
+            ret_data["message"]="新用户名已被使用！"
+        else:
+            #更新user
+            user.username=req_data["username"]
+            user.email=req_data["email"]
+            User_Update(user)
+            #修改ret数据
+            ret_data["iscorrect"]=True
+    return jsonify(ret_data)
