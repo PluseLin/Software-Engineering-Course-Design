ae14e3fdf813aaf18c01da78eaa0f873dd891b93
PluseLin <1390297535@qq.com>
2022-08-14 18:35:40 +0800

    add flaskcors

--- flask_server/app/__init__.py
+++ flask_server/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import *
 from flask_sqlalchemy import *
+from flask_cors import *
 from config import config
 
 db=SQLAlchemy()
@@ -8,6 +9,8 @@ def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    CORS(app,resources={r'/*':{'origins':'*'}})
 
     db.init_app(app)
     
