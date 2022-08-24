bcb922d6324b6ab213a840d36566018c8a36aadd
lileyang <lileyang10@sina.com>
2022-08-24 11:16:24 +0800

    sourcecode

---
+++ vue_client/src(lly)/App.vue
@@ -0,0 +1,20 @@
+<template>
+<router-view></router-view>
+
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld';
+
+/*export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};*/
+</script>
