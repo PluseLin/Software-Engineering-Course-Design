b4f355e500fdd5b8c5d470438f2f93df12cd57e2
lileyang <lileyang10@sina.com>
2022-08-25 23:00:49 +0800

    this is version_2

--- flask_server/app/main/views.py
+++ flask_server/app/main/views.py
@@ -10,6 +10,7 @@ import time
 
 @main.route("/userLogin",methods=['POST'])
 def login():
+    global session
     ret_data={
         "iscorrect":False,
         "message":"",
@@ -22,25 +23,28 @@ def login():
             ret_data["message"]="用户名或密码错误"
             ret_data["url"]="#"
         else:
-            session["username"]=True
+            session[username]=True
+            session.permanent=True
             ret_data["iscorrect"]=True
             ret_data["url"]="/{}/index".format(username) #可能需要修改
     return jsonify(ret_data)
 
-@main.route("/userLogout",methods=['POST'])
-def logout():
-    ret_data={
-        "issuccess":False,
-    }
-    if request.method=='POST':
-        req_data=request.get_json()
-        username=req_data["username"]
-        if session[username]:
-            session[username]=False
-            ret_data["issuccess"]=True
-        else:
-            ret_data["issuccess"]=False
-    return ret_data
+# @main.route("/userLogout",methods=['POST'])
+# def logout():
+#     global session
+#     ret_data={
+#         "issuccess":False,
+#     }
+#     if request.method=='POST':
+#         print(session)
+#         req_data=request.get_json()
+#         username=req_data["username"]
+#         if session.get(username)==True:
+#             session[username]=False
+#             ret_data["issuccess"]=True
+#         else:
+#             ret_data["issuccess"]=False
+#     return ret_data
 
 @main.route("/userRegister",methods=['POST'])
 def register():
@@ -48,7 +52,6 @@ def register():
         "iscorrect":False,
         "message":""
     }
-    print(request.method)
     if(request.method=='POST'):
         req_data=request.get_json()
         username=req_data["username"]
@@ -75,12 +78,12 @@ def register():
 @main.route("/getUserInfo_<username>",methods=['GET'])
 def getUserInfo(username):
     ret_data={
-        "id":0,
+        "id":-1,
         "username":username,
         "password":"",
         "email":"",
         "regtime":"",
-        "ip_addr":request.remote_addr
+        "ip_addr":""
     }
     if request.method=='GET':
         user=User_Query_by_username(username)
@@ -89,10 +92,36 @@ def getUserInfo(username):
             ret_data["email"]=user.email
             ret_data["regtime"]=user.reg_time
             ret_data["id"]=user.id
+            ret_data["ip_addr"]=request.remote_addr
     return jsonify(ret_data)
 
-@main.route("/updateUserInfo",methods=['PUT'])
-def updateUserInfo():
+@main.route("/updateUsername",methods=['PUT'])
+def updateUsername():
+    ret_data={
+        "iscorrect":False,
+        "message":""
+    }
+    if request.method=="PUT":
+        req_data=request.get_json()
+        old_username=req_data["old_username"]
+        new_username=req_data["new_username"]
+        user=User_Query_by_username(old_username)
+        #首先，检查user是否存在
+        if user is None:
+            ret_data["message"]="用户不存在！"
+        #其次，检查用户名是否被占用
+        elif User_Query_by_username(new_username) is not None:
+            ret_data["message"]="用户名已被使用！"
+        #若都符合，则修改
+        else:
+            user.username=new_username
+            User_Update(user)
+            ret_data["iscorrect"]=True
+            ret_data["message"]="修改用户名成功！"
+    return jsonify(ret_data)
+
+@main.route("/updateUserEmail",methods=['PUT'])
+def updateUserEmail():
     ret_data={
         "iscorrect":False,
         "message":""
@@ -100,21 +129,17 @@ def updateUserInfo():
     if request.method=="PUT":
         req_data=request.get_json()
         username=req_data["username"]
-        user_id=req_data["user_id"]
-        user=User_Query_by_id(user_id)
-        #首先，保证用户id存在
+        new_email=req_data["new_email"]
+        user=User_Query_by_username(username)
+        #首先，检查user是否存在
         if user is None:
             ret_data["message"]="用户不存在！"
-        #其次，保证新用户名存在
-        elif User_Query_by_username(username) is not None:
-            ret_data["message"]="新用户名已被使用！"
+        #若符合，则修改
         else:
-            #更新user
-            user.username=req_data["username"]
-            user.email=req_data["email"]
+            user.email=new_email
             User_Update(user)
-            #修改ret数据
             ret_data["iscorrect"]=True
+            ret_data["message"]="修改成功！"
     return jsonify(ret_data)
 
 @main.route("/updateUserPassword",methods=['PUT'])
@@ -126,11 +151,19 @@ def updateUserPassword():
     if request.method=="PUT":
         req_data=request.get_json()
         username=req_data["username"]
-        new_password=req_data["password"]
+        old_password=req_data["old_password"]
+        new_password=req_data["new_password"]
+        new_repassword=req_data["new_repassword"]
         user=User_Query_by_username(username)
         #首先，保证用户存在
         if user is None:
             ret_data["message"]="用户不存在！"
+        #其次，旧密码应该正确
+        elif user.password!=old_password:
+            ret_data["message"]="旧密码不正确！"
+        #再其次，两次密码应该一致
+        elif new_password!=new_repassword:
+            ret_data["message"]="两次确认新密码不一致！"
         else:
             #更新user的password
             user.password=new_password
